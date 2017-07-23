33651c6f6b03a0ce3c3ee9c1ee8baf164973bbd3
GoranKovac <gogokily@gmail.com>
2017-07-23 22:20:58 +0200

    Update Sexan_select all other items on track.lua

--- Items/Sexan_select all other items on track.lua
+++ Items/Sexan_select all other items on track.lua
@@ -5,17 +5,20 @@
  * Licence: GPL v3
  * REAPER: 5.0
  * Extensions: None
- * Version: 1.11
+ * Version: 1.12
 --]]
  
 --[[
  * Changelog:
- * v1.11 (2017-07-23)
-  + Initial release
+ * v1.12 (2017-07-23)
+  + fixed error
 --]]
 
 function Main()
   local sel_item = reaper.GetSelectedMediaItem( 0, 0 )
+  
+  if not sel_item then return 0 end
+  
   local tr =  reaper.GetMediaItem_Track( sel_item )
   reaper.SetMediaItemInfo_Value( sel_item, "B_UISEL", 0 )
   
