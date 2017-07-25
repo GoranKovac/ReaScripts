89287dd8f974355ad0d5ce685add6212bb63fbb5
GoranKovac <gogokily@gmail.com>
2017-07-25 13:06:07 +0200

    Update Sexan_Set Item and take volume of previous odd to next even items.lua

--- Items/Sexan_Set Item and take volume of previous odd to next even items.lua
+++ Items/Sexan_Set Item and take volume of previous odd to next even items.lua
@@ -5,17 +5,40 @@
  * Licence: GPL v3
  * REAPER: 5.0
  * Extensions: None
- * Version: 1.0
+ * Version: 1.1
 --]]
  
 --[[
  * Changelog:
- * v1.0 (2017-07-24)
-  + Initial Release
+ * v1.1 (2017-07-25)
+  + Added Envelope and always run on track 3
 --]]
 
-function Main()  
-  local tr =  reaper.GetSelectedTrack(0,0)
+-- Aquired from SPK77 Script - Copy take volume envelope from selected take to other takes in same group
+function get_and_show_take_envelope(take, envelope_name)
+  local env = reaper.GetTakeEnvelopeByName(take, envelope_name)
+  if env == nil then
+    local item = reaper.GetMediaItemTake_Item(take)
+    local sel = reaper.IsMediaItemSelected(item)
+    if not sel then
+      reaper.SetMediaItemSelected(item, true)
+    end
+    if     envelope_name == "Volume" then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_TAKEENV1"), 0) -- show take volume envelope
+    elseif envelope_name == "Pan" then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_TAKEENV2"), 0)    -- show take pan envelope
+    elseif envelope_name == "Mute" then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_TAKEENV3"), 0)   -- show take mute envelope
+    elseif envelope_name == "Pitch" then reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_TAKEENV10"), 0) -- show take pitch envelope
+    end
+    if sel then
+      reaper.SetMediaItemSelected(item, false)
+    end
+    env = reaper.GetTakeEnvelopeByName(take, envelope_name)
+  end
+  return env
+end
+
+function Main() 
+  -- ALWAYS USE TRACK 3 
+  local tr =  reaper.GetTrack(0,2)
   
   if not tr then return 0 end
   
@@ -24,17 +47,24 @@ function Main()
     for i = 1, cnt_items, 2 do
       -- ODD
       local o_item =  reaper.GetTrackMediaItem( tr, i-1 )
-      local o_take = reaper.GetActiveTake(o_item)
       local o_item_v =  reaper.GetMediaItemInfo_Value( o_item, "D_VOL" )
+      
+      local o_take = reaper.GetActiveTake(o_item)      
       local o_take_v =  reaper.GetMediaItemTakeInfo_Value( o_take, "D_VOL" )
+      local o_take_e = get_and_show_take_envelope(o_take, "Volume")
+      local retval, env_chunk = reaper.GetEnvelopeStateChunk( o_take_e, "", true )
       
       i = i + 1
       
       -- EVEN
       local e_item =  reaper.GetTrackMediaItem( tr, i-1 )
-      local e_take = reaper.GetActiveTake(e_item)
       local e_item_v = reaper.SetMediaItemInfo_Value( e_item, "D_VOL", o_item_v )
+      
+      local e_take = reaper.GetActiveTake(e_item)      
       local e_take_v = reaper.SetMediaItemTakeInfo_Value( e_take, "D_VOL", o_take_v )
+      local env = get_and_show_take_envelope(e_take, "Volume")
+      local set_env = reaper.SetEnvelopeStateChunk(env, env_chunk, true)
+            
     end
     
   reaper.UpdateArrange()
