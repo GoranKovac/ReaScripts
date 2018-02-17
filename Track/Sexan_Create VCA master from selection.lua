bab53784379d569759d2df63d238300aefcb5b3f
GoranKovac <gogokily@gmail.com>
2018-02-17 16:18:28 +0100

    64Group
    64 group support,new position

--- Track/Sexan_Create VCA master from selection.lua
+++ Track/Sexan_Create VCA master from selection.lua
@@ -5,32 +5,24 @@
  * Licence: GPL v3
  * REAPER: 5.0
  * Extensions: None
- * Version: 1.453
+ * Version: 1.51
 --]]
  
 --[[
  * Changelog:
- * v1.453 (2017-07-13)
-  + When clicking cancel or pressing ESC key in naming popup do nothing
+ * v1.51 (2018-02-17)
+  + Better code, added option to add VCA above selected track, 64 groups support
 --]]
 
 -- USER SETTING
 ---------------
-popup = 0    -- (set to 0 for no popup, set to 1 for popup asking to name the VCA group)
-mute_solo = 1 -- (set to 0 to disable mute and solo flags)
-position = 1 -- (set VCA Master track position 1 - TOP , 0 - Bottom)
-warning = 0  -- (gives user a warning popup to select tracks if no tracks are selected, 0 OFF , 1 ON)
+local popup = 0    -- (set to 0 for no popup, set to 1 for popup asking to name the VCA group)
+local mute_solo = 1 -- (set to 0 to disable mute and solo flags)
+local position = 3 -- (set VCA Master track position 1 - TOP , 0 - Bottom, 3 - Above selected tracks)
+local warning = 0  -- (gives user a warning popup to select tracks if no tracks are selected, 0 OFF , 1 ON)
 ---------------
 --------------------------------------------------------------------------------------
 -- GROUP FLAGS
-local groups =  { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
-                  131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864,
-                  134217728, 268435456, 536870912, 1073741824, 2147483648 
-                } 
-local unused =  { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,                  
-                  131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864,                  
-                  134217728, 268435456, 536870912, 1073741824, 2147483648 
-                }                
 
 local VCA_FLAGS = { "VOLUME_MASTER", 
                     "VOLUME_SLAVE", 
@@ -50,43 +42,44 @@ local VCA_FLAGS = { "VOLUME_MASTER",
                     "POLARITY_SLAVE", 
                     "AUTOMODE_MASTER",
                     "AUTOMODE_SLAVE"
-                  }  
-local vca_group = {}                                              
-local tracks = {}
-local top_pos_offset
+                  }
+local free_group,master_pos = nil, nil 
+local tracks, vca_group ,cnt = {}, {}, 1 
+for i = 1 ,64 do vca_group[i] = 0 end
 
 local function scan_groups()
   local cnt_tr = reaper.CountTracks(0)
-  -- SCAN FOR ALL FLAGS
   for i = 0 , cnt_tr-1 do
     local tr = reaper.GetTrack(0,i)
-    local VCA_M = reaper.GetSetTrackGroupMembership(tr,"VOLUME_VCA_MASTER", 0,0)
+    for k = 1 , #vca_group do
+      if reaper.GetSetTrackGroupMembership(tr,"VOLUME_VCA_MASTER", 0,0) == 2^(k-1) or reaper.GetSetTrackGroupMembershipHigh(tr,"VOLUME_VCA_MASTER", 0,0) == 2^((k-32)-1) then cnt = cnt + 1 end
       for j = 1 , #VCA_FLAGS do
-        local flags = reaper.GetSetTrackGroupMembership(tr,VCA_FLAGS[j], 0,0)
-          for k = 1 , #groups do
-            -- IF GROUP IS USED REMOVE IT FROM UNUSED TABLE
-            if flags == unused[k] then
-              table.remove(unused,k)
-            end
-          end
-      end
-      --SCAN VCA MASTER FLAGS ONLY AND ADD IT TO GROUP(NEEDED FOR TRACK CREATION ON TOP)
-      for l = 1 , #groups do
-        if VCA_M == groups[l] then
-          vca_group[#vca_group+1] = groups[l]            
+        if reaper.GetSetTrackGroupMembership(tr,VCA_FLAGS[j], 0,0) == 2^(k-1) or 
+          reaper.GetSetTrackGroupMembershipHigh(tr,VCA_FLAGS[j], 0,0) == 2^((k-32)-1) then 
+          vca_group[k] = nil
         end
-      end      
+      end
+    end
   end
-  top_pos_offset = #vca_group
 end
  
-local function create_VCAs()
-  local free_group = unused[#unused]
+function create_VCAs()
+ local group 
+ for k,v in pairs(vca_group) do
+   if v == 0 then 
+     if k > 32 then group = reaper.GetSetTrackGroupMembershipHigh free_group = 2^((k-32)-1)
+     else group = reaper.GetSetTrackGroupMembership free_group = 2^(k-1)
+     end
+     --break -- ADD BREAK TO ITERIATE FROM 1 to 32, without its 32 to 1
+    end
+  end
   
   if position == 1 then
-    master_pos = top_pos_offset
-  else
+    master_pos = cnt - 1
+  elseif position == 0 then
     master_pos = reaper.CountTracks(0)
+  else
+    master_pos = reaper.CSurf_TrackToID( tracks[1], false ) - 1 -- ADD VCA ABOVE SELECTED TRACKS
   end
   
   -- INSERT MASTER VCA TRACK AT TOP OR BOTTTOM
@@ -96,7 +89,7 @@ local function create_VCAs()
   
   -- VCA NAMING
   if popup == 0 then
-    local retval, track_name = reaper.GetSetMediaTrackInfo_String(tr, "P_NAME", "VCA " .. #vca_group+1 , true)
+    local retval, track_name = reaper.GetSetMediaTrackInfo_String(tr, "P_NAME", "VCA " .. cnt , true)
   else
     local ret , name = reaper.GetUserInputs("ADD VCA NAME ", 1, "VCA NAME :", "")
     -- IF OK WAS CLICKED
@@ -108,33 +101,32 @@ local function create_VCAs()
       return 0
     end
   end
-  
+ 
   -- SET TRACK AS VCA MASTER
-  local VCA_M = reaper.GetSetTrackGroupMembership(tr,"VOLUME_VCA_MASTER", free_group,free_group)
+  local VCA_M = group(tr,"VOLUME_VCA_MASTER", free_group,free_group)
     if mute_solo == 1 then 
-      local VCA_M_MUTE = reaper.GetSetTrackGroupMembership(tr,"MUTE_MASTER", free_group,free_group)
-      local VCA_M_SOLO = reaper.GetSetTrackGroupMembership(tr,"SOLO_MASTER", free_group,free_group)
+      local VCA_M_MUTE = group(tr,"MUTE_MASTER", free_group,free_group)
+      local VCA_M_SOLO = group(tr,"SOLO_MASTER", free_group,free_group)
     end
     
   -- SET VCA SLAVES  
   for i = 1, #tracks do
     local tr = tracks[i]
-    local VCA_S = reaper.GetSetTrackGroupMembership(tr,"VOLUME_VCA_SLAVE", free_group,free_group)
+    local VCA_S = group(tr,"VOLUME_VCA_SLAVE", free_group,free_group)
       if mute_solo == 1 then
-        local VCA_S_MUTE = reaper.GetSetTrackGroupMembership(tr,"MUTE_SLAVE", free_group,free_group)
-        local VCA_S_SOLO = reaper.GetSetTrackGroupMembership(tr,"SOLO_SLAVE", free_group,free_group)
+        local VCA_S_MUTE = group(tr,"MUTE_SLAVE", free_group,free_group)
+        local VCA_S_SOLO = group(tr,"SOLO_SLAVE", free_group,free_group)
       end
   end
 end
 
 local function main()
-  local free_group
   local cnt_sel = reaper.CountSelectedTracks(0)
   if warning == 1 and cnt_sel == 0 then
     reaper.ShowMessageBox("Please select tracks to create VCA", "WARNING", 0)
   end
-  -- IF UNUSED GROUP TABLE IS EMPTY DO NOT CREATE NEW GROUP
-  if #unused ~= 0 and cnt_sel > 0 then 
+  -- IF VCA GROUP TABLE IS EMPTY DO NOT CREATE NEW GROUP
+  if #vca_group ~= 0 and cnt_sel > 0 then 
     -- ADD SELECTED TRACKS TO TABLE (FOR MAKING THEM VCA SLAVES)
     for i = 0, cnt_sel-1 do
       local tr = reaper.GetSelectedTrack(0,i)
