c6910bc30cb0bbca6014ea8fd22fe2694eed0560
GoranKovac <gogokily@gmail.com>
2018-02-27 15:40:19 +0100

    64 group support
    64 group support

--- Track/Sexan_Remove selected VCA master and its slaves with all flags.lua
+++ Track/Sexan_Remove selected VCA master and its slaves with all flags.lua
@@ -5,25 +5,27 @@
  * Licence: GPL v3
  * REAPER: 5.0
  * Extensions: None
- * Version: 1.02
+ * Version: 1.03
 --]]
  
 --[[
  * Changelog:
- * v1.02 (2017-07-13)
-  + Better Script name
+ * v1.03 (2018-02-27)
+  + Fixed for 64 group remove
 --]]
 
 --------------------------------------------------------------------------------------
+local tr_group = reaper.GetSetTrackGroupMembership
+
 local function remove_slave_flags(VCA_GROUP)
   local cnt_tr = reaper.CountTracks(0)
   
   for i = 0 , cnt_tr-1 do
     local tr = reaper.GetTrack(0,i)
     -- REMOVE ALL SLAVES FROM GROUP 
-    local VCA_S = reaper.GetSetTrackGroupMembership(tr,"VOLUME_VCA_SLAVE", VCA_GROUP,0)
-    local VCA_S_MUTE = reaper.GetSetTrackGroupMembership(tr,"MUTE_SLAVE", VCA_GROUP,0)
-    local VCA_S_SOLO = reaper.GetSetTrackGroupMembership(tr,"SOLO_SLAVE", VCA_GROUP,0)
+    local VCA_S =tr_group(tr,"VOLUME_VCA_SLAVE", VCA_GROUP,0)
+    local VCA_S_MUTE =tr_group(tr,"MUTE_SLAVE", VCA_GROUP,0)
+    local VCA_S_SOLO = tr_group(tr,"SOLO_SLAVE", VCA_GROUP,0)
   end
   
 end
@@ -31,11 +33,12 @@ end
 local function remove_master_flags()
   local sel_tr = reaper.GetSelectedTrack(0,0)
   -- Find ACTIVE GROUP
-  local VCA_GROUP = reaper.GetSetTrackGroupMembership(sel_tr,"VOLUME_VCA_MASTER", 0, 0)
+  if tr_group(sel_tr,"VOLUME_VCA_MASTER", 0, 0) == 0 then tr_group = reaper.GetSetTrackGroupMembershipHigh end
+  local VCA_GROUP = tr_group(sel_tr,"VOLUME_VCA_MASTER", 0, 0)
   -- REMOVE VCA MASTER AND GROUP
-  local VCA_M = reaper.GetSetTrackGroupMembership(sel_tr,"VOLUME_VCA_MASTER", VCA_GROUP, 0)
-  local VCA_M_MUTE = reaper.GetSetTrackGroupMembership(sel_tr,"MUTE_MASTER", VCA_GROUP,0)
-  local VCA_M_SOLO = reaper.GetSetTrackGroupMembership(sel_tr,"SOLO_MASTER", VCA_GROUP,0)
+  local VCA_M = tr_group(sel_tr,"VOLUME_VCA_MASTER", VCA_GROUP, 0)
+  local VCA_M_MUTE = tr_group(sel_tr,"MUTE_MASTER", VCA_GROUP,0)
+  local VCA_M_SOLO = tr_group(sel_tr,"SOLO_MASTER", VCA_GROUP,0)
   remove_slave_flags(VCA_GROUP)
 end
 remove_master_flags()
