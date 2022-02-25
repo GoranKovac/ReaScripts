df8ecd835b5360fefa7ae8fe1b7bd1ca957cffa9
GoranKovac <gorankovacstudio@gmail.com>
2022-02-25 21:23:35 +0100

    lil fixup

--- VirtualTrack/Virtual_track.lua
+++ VirtualTrack/Virtual_track.lua
@@ -154,12 +154,12 @@ local function Exclude_Pattern(chunk)
     return chunk
 end
 
-local function Get_folder(tr)
-    if reaper.GetMediaTrackInfo_Value(tr, "I_FOLDERDEPTH") <= 0 then
+local function Get_folder(track)
+    if reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH") <= 0 then
         return
     end
     local depth, children = 0, {}
-    local folderID = reaper.GetMediaTrackInfo_Value(tr, "IP_TRACKNUMBER") - 1
+    local folderID = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER") - 1
     for i = folderID + 1, reaper.CountTracks(0) - 1 do
         local child = reaper.GetTrack(0, i)
         local currDepth = reaper.GetMediaTrackInfo_Value(child, "I_FOLDERDEPTH")
