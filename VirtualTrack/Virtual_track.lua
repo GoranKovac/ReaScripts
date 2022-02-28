503e1ad5d205e858a58325c0b2f1b9cbe77c95a6
GoranKovac <gorankovacstudio@gmail.com>
2022-02-28 18:08:37 +0100

    Y NO UPDATE

--- VirtualTrack/Virtual_track.lua
+++ VirtualTrack/Virtual_track.lua
@@ -4,13 +4,13 @@
  * Licence: GPL v3
  * REAPER: 6.0
  * Extensions: None
- * Version: 0.41
+ * Version: 0.42
  * Provides: Modules/*.lua
 --]]
 
 --[[
  * Changelog:
- * v0.41 (2022-02-28)
+ * v0.42 (2022-02-28)
    + Move Button to TCP
    + Added master track versions
    + Tempo map chunk template
