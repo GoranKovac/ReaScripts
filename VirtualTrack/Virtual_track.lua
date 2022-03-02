10d9df5f34404d377baa060a9ee9a2cc4b6b186f
GoranKovac <gorankovacstudio@gmail.com>
2022-03-02 08:44:31 +0100

    last changelog test

--- VirtualTrack/Virtual_track.lua
+++ VirtualTrack/Virtual_track.lua
@@ -4,10 +4,9 @@
  * Licence: GPL v3
  * REAPER: 6.0
  * Extensions: None
- * Version: 0.52
- * Changelog:
-   + fixed crash when hovering over mixer master track
-   + Added button icon instead of rendering color rectangle
+ * Version: 0.53
+ * Changelog: + fixed crash when hovering over mixer master track
+              + Added button icon instead of rendering color rectangle
  * Provides: Modules/*.lua
 --]]
 
