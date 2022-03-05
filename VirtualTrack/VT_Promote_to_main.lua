68c2924086f201e6ef47206b38a855b7600458ee
GoranKovac <gorankovacstudio@gmail.com>
2022-03-05 12:47:07 +0100

    forgot this one

--- VirtualTrack/VT_Promote_to_main.lua
+++ VirtualTrack/VT_Promote_to_main.lua
@@ -9,17 +9,6 @@ local reaper = reaper
 
 package.path = debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" -- GET DIRECTORY FOR REQUIRE
 
-if not reaper.APIExists("JS_ReaScriptAPI_Version") then
-    reaper.MB( "JS_ReaScriptAPI is required for this script", "Please download it from ReaPack", 0 )
-    return reaper.defer(function() end)
-else
-    local version = reaper.JS_ReaScriptAPI_Version()
-    if version < 1.002 then
-        reaper.MB( "Your JS_ReaScriptAPI version is " .. version .. "\nPlease update to latest version.", "Older version is installed", 0 )
-        return reaper.defer(function() end)
-    end
-end
-
 require("Modules/VTCommon")
 require("Modules/Class")
 require("Modules/Mouse")
