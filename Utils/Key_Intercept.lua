b4f4be8a73d5d16246f5fbc60ffb851821a68020
GoranKovac <gorankovacstudio@gmail.com>
2022-04-16 13:33:56 +0200

    minimal code for key intercept script defer

---
+++ Utils/Key_Intercept.lua
@@ -0,0 +1,22 @@
+local reaper = reaper
+local startTime = reaper.time_precise()
+local key_state = reaper.JS_VKeys_GetState(startTime - 2)
+for i = 1, 255 do
+    if key_state:byte(i) ~= 0 then KEY = i; reaper.JS_VKeys_Intercept(KEY, 1) --[[ reaper.ShowConsoleMsg(string.char(i)) ]] end
+end
+
+function Kill_script()
+    key_state = reaper.JS_VKeys_GetState(startTime - 2)
+    reaper.ShowConsoleMsg("SCRIPT RUNNING")
+    if key_state:byte(KEY) == 0 then reaper.JS_VKeys_Intercept(KEY, -1) return true end
+end
+
+function Main()
+    if Kill_script() then return end
+    reaper.defer(Main)
+end
+
+function atExit() reaper.JS_VKeys_Intercept(-1, -1) end -- JUST IN CASE RELEASE ALL KEYS
+
+reaper.atexit(atExit)
+Main()
