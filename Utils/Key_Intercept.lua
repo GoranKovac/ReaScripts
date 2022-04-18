c3acb058e2149ea5708cb049194386299059cfe3
GoranKovac <gorankovacstudio@gmail.com>
2022-04-18 08:21:52 +0200

    always allow keyboard while script are running

--- Utils/Key_Intercept.lua
+++ Utils/Key_Intercept.lua
@@ -1,17 +1,20 @@
 local reaper = reaper
+
 local start_time = reaper.time_precise()
 local key_state, KEY = reaper.JS_VKeys_GetState(start_time - 2), nil
 for i = 1, 255 do
     if key_state:byte(i) ~= 0 then KEY = i; reaper.JS_VKeys_Intercept(KEY, 1) break end
 end
 if not KEY then return end
+local cur_pref = reaper.SNM_GetIntConfigVar("alwaysallowkb", 1)
+reaper.SNM_SetIntConfigVar("alwaysallowkb", 1)
 
 function Key_held()
     key_state = reaper.JS_VKeys_GetState(start_time - 2)
     return key_state:byte(KEY) == 1
 end
 
-function Release() reaper.JS_VKeys_Intercept(KEY, -1) end
+function Release() reaper.JS_VKeys_Intercept(KEY, -1) reaper.SNM_SetIntConfigVar("alwaysallowkb", cur_pref) end
 
 function Handle_errors(err)
     reaper.ShowConsoleMsg(err .. '\n' .. debug.traceback())
