b01df93939d4100c29fa72cc40d49cd6f3e11e5f
GoranKovac <gorankovacstudio@gmail.com>
2024-02-12 11:18:33 +0100

    Don't save absolute paths, some minor changes

--- ImGui_Tools/PluginScreenShooter.lua
+++ ImGui_Tools/PluginScreenShooter.lua
@@ -3,6 +3,8 @@
 local r = reaper
 local deps = {}
 
+local proc_delay = 0.5
+local SIZE_DATA = {}
 if not r.ImGui_GetVersion then
     deps[#deps + 1] = '"Dear Imgui"'
 end
@@ -17,88 +19,170 @@ if #deps ~= 0 then
 end
 
 local ctx = r.ImGui_CreateContext('FX SCREENSHOTER')
-local folder = r.GetResourcePath() ..  "\\Scripts\\FX_PNG\\"
-
-if not r.file_exists( folder ) then
-  os.execute("mkdir " .. folder)
+local png_folder = "\\Scripts\\FX_PNG\\"
+local folder = r.GetResourcePath() .. png_folder
+if not r.file_exists(folder) then
+    os.execute("mkdir " .. folder)
 end
 
-local tr_idx = r.InsertTrackAtIndex( r.CountTracks(0), false )
-local track = r.GetTrack(tr_idx, 0)
+r.InsertTrackAtIndex(r.CountTracks(0), false)
+local track = r.GetTrack(r.CountTracks(0), 0)
 local i = 0
-local proc_delay = 0.5
 
-function Wait(startTime, callback)
-  PROCESS = true
-  if r.time_precise() - startTime > proc_delay then
-      callback()
-      i = i + 1
-      PROCESS = nil
-  else
-    r.defer(function() Wait(startTime, callback) end)
-  end
+function SerializeToFile(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+    local tmp = string.rep(" ", depth)
+    if name then
+        if type(name) == "number" and math.floor(name) == name then
+            name = "[" .. name .. "]"
+        elseif not string.match(name, '^[a-zA-z_][a-zA-Z0-9_]*$') then
+            name = string.gsub(name, "'", "\\'")
+            name = "['" .. name .. "']"
+        end
+        tmp = tmp .. name .. " = "
+    end
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+        for k, v in pairs(val) do
+            tmp = tmp .. SerializeToFile(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        --! THIS IS MODIFICATION FOR THIS SCRIPT
+        --! POINTERS GET RECALCULATED ON RUN SO WE NIL HERE (MEDIATRACKS, MEDIAITEMS... )
+        tmp = tmp .. "nil"
+        --tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+    return tmp
 end
 
-local function takeScreenshot()
-    local window = r.TrackFX_GetFloatingWindow(track, fxIndex )
-    local retval, w, h = r.JS_Window_GetClientSize( window )
-    if retval then
-        local srcDC = r.JS_GDI_GetWindowDC(window)
-        local destBmp = r.JS_LICE_CreateBitmap(true, w, h)
-        local destDC = r.JS_LICE_GetDC(destBmp)
-        r.JS_GDI_Blit(destDC, 0, 0, srcDC, 8, 31, w, h)
-        r.JS_LICE_WritePNG(path, destBmp, false)
-        r.JS_GDI_ReleaseDC(window, srcDC)
-        r.JS_LICE_DestroyBitmap(destBmp)
-        r.TrackFX_Delete(track, fxIndex) 
+function StringToTable(str)
+    local f, err = load("return " .. str)
+    if err then
+        reaper.ShowConsoleMsg("\nerror" .. err)
+    end
+    return f ~= nil and f() or nil
+end
+
+function TableToString(table) return SerializeToFile(table) end
+
+function WriteToFile(path, data)
+    local file_cat = io.open(path, "w")
+    if file_cat then
+        file_cat:write(data)
+        file_cat:close()
     end
 end
 
-function main()
-    --r.ShowConsoleMsg(i.."\n")
-    retval, pluginName, ident = r.EnumInstalledFX(i)
+local function WriteFxData()
+    local serialized = TableToString(SIZE_DATA)
+    WriteToFile(folder .. "FX_SIZES.txt", serialized)
+end
+
+function Wait(startTime, callback)
+    PROCESS = true
+    if r.time_precise() - startTime > proc_delay then
+        callback()
+        i = i + 1
+        PROCESS = nil
+    else
+        r.defer(function() Wait(startTime, callback) end)
+    end
+end
+
+local function FxOffset()
+    return pluginName:match("^JS") and 0 or 27
+end
+
+local function takeScreenshot(fxIndex, path)
+    local window = r.TrackFX_GetFloatingWindow(track, fxIndex)
+    local retval, w, h = r.JS_Window_GetClientSize(window)
+    if retval then
+        local off_y = FxOffset()
+        local srcDC = r.JS_GDI_GetClientDC(window)
+        local destBmp = r.JS_LICE_CreateBitmap(true, w, h - off_y)
+        local destDC = r.JS_LICE_GetDC(destBmp)
+        r.JS_GDI_Blit(destDC, 0, -off_y, srcDC, 0, 0, w, h)
+        r.JS_LICE_WritePNG(path .. ".png", destBmp, false)
+        r.JS_GDI_ReleaseDC(window, srcDC)
+        r.JS_LICE_DestroyBitmap(destBmp)
+        if r.ValidatePtr(track, "MediaTrack*") then
+            r.TrackFX_Delete(track, fxIndex)
+        end
+        SIZE_DATA[pluginName] = { path = path .. ".png", w = w, h = h }
+    end
+end
+
+function Main()
+    if DONE then return end
+    retval, pluginName = r.EnumInstalledFX(i)
     if not retval then
         START = false
         DONE = true
+        WriteFxData()
+        return
     end
-    if pluginName then
-      -- REPLACE SYMBOLS WITH _ FOR FILE WRITING
-      local pluginName_strip = pluginName:gsub("[-:_/%s><]", "_")
-      path = folder .. pluginName_strip  .. '.png'
-      -- IF FILE EXIST SKIP IT
-      if not r.file_exists( path ) then
-        if not PROCESS then
-        fxIndex = r.TrackFX_AddByName(track, pluginName, false, 1)
-        r.TrackFX_Show(track, fxIndex, 3)
-          Wait(
-            r.time_precise(), 
-            function()
-              takeScreenshot()
+    if pluginName and pluginName:match("Cockos") then
+        local png_name = pluginName:gsub("[-:_/%s><]", "_")
+        local path = folder .. png_name
+        if not r.file_exists(path .. ".png") then
+            if not PROCESS then
+                local fxIndex = r.TrackFX_AddByName(track, pluginName, false, 1)
+                r.TrackFX_Show(track, fxIndex, 3)
+                Wait(
+                    r.time_precise(),
+                    function()
+                        takeScreenshot(fxIndex, path)
+                    end
+                )
             end
-          )
+        else
+            i = i + 1
         end
-      else
-        i = i + 1
-      end
     else
-      i = i + 1
+        i = i + 1
     end
+end
+
+local function BuildDatabase()
+    SIZE_DATA = {}
+    for j = 1, math.huge do
+        local retval, pluginName = r.EnumInstalledFX(j)
+        if not retval then break end
+        local png_name = pluginName:gsub("[-:_/%s><]", "_")
+        local path = folder .. png_name .. ".png"
+        if r.file_exists(path) then
+            local img = r.ImGui_CreateImage(path)
+            local w, h = r.ImGui_Image_GetSize(img)
+            SIZE_DATA[pluginName] = { path = png_folder .. png_name .. ".png", w = w, h = h }
+        end
+    end
+    WriteFxData()
 end
 
 local function gui()
     local visible, open = r.ImGui_Begin(ctx, 'FX SCREENSHOTER', true)
     if visible then
-        if r.ImGui_Button( ctx, START and "STOP" or "START" ) then
-          if START then
-            START = false
-          else
-            START = true
-          end
+        if r.ImGui_Button(ctx, START and "STOP" or "START") then
+            START = not START
+            if DONE then DONE = nil end
         end
         r.ImGui_SameLine(ctx)
-       
-        if START then main() end
-        r.ImGui_Text(ctx, DONE and "FINISHED" or ("PROCESSING : ID " .. i .. " - " .. (pluginName or "")))
+
+        if START then Main() end
+        r.ImGui_Text(ctx, DONE and "FINISHED" or (START and "PROCESSING : ID " .. i .. " - " .. (pluginName or "") or ""))
+        if not START then
+            if r.ImGui_Button(ctx, "BUILD PNG DATABASE") then
+                BuildDatabase()
+            end
+        end
         r.ImGui_End(ctx)
     end
 
@@ -108,7 +192,9 @@ local function gui()
 end
 
 function Exit()
-  if track then r.DeleteTrack( track )  end
+    if track and r.ValidatePtr(track, "MediaTrack*") then
+        r.DeleteTrack(track)
+    end
 end
 
 r.atexit(Exit)
