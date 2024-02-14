175835843a2af35feb3609d3db21d28b0c10e29a
GoranKovac <gorankovacstudio@gmail.com>
2024-02-14 13:30:42 +0100

    fix2

--- Track/Sexan_Create VCA master from selection.lua
+++ Track/Sexan_Create VCA master from selection.lua
@@ -1,7 +1,7 @@
 
 -- @description Create VCA Master from selection.lua
 -- @author SeXan
--- @about  Script creates Master VCA for selected tracks and makes them VCA Slave.Also Mute and SOLO flags are added
+-- @about Script creates Master VCA for selected tracks and makes them VCA Slave.Also Mute and SOLO flags are added
 -- @license GPL v3
 -- @version 1.711
 -- @changelog
