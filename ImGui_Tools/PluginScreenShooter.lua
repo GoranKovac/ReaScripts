a8445988f8e90e441ecd86052aff952c98fd93da
Goran Kovac <gorankovacstudio@gmail.com>
2024-08-20 22:30:22 +0200

    one more with fix

--- ImGui_Tools/PluginScreenShooter.lua
+++ ImGui_Tools/PluginScreenShooter.lua
@@ -136,7 +136,7 @@ local function takeScreenshot(fxIndex, path)
             r.JS_LICE_DestroyBitmap(destBmp)
         else
             h = top - bottom
-            destBmp = ScreenshotOSX(path, right, top, left, h - off_y)
+            destBmp = ScreenshotOSX(path, right, top, w, h - off_y)
         end
         if r.ValidatePtr(track, "MediaTrack*") then
             r.TrackFX_Delete(track, fxIndex)
